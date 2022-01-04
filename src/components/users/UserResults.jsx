fe841ba51abe9868529d9de9a5963a46b7de5582
hafizjavaid <hafizjaidi786@gmail.com>
2022-01-04 22:19:10 +0500

    Finishing Basic Context and Adding use Reducer

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,29 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import GithubContext from "../../context/GithubContext";
 // import Spinner from "../layout/Spinner";
 import UserItem from "../users/UserItem";
 
 function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-
+  const { users, loading, fetchUsers } = useContext(GithubContext);
   useEffect(() => {
     fetchUsers();
-  }, []);
-
-  const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_TOKEN}`,
-      },
-    });
-
-    console.log(response);
-    const data = await response.json();
-    console.log(data);
-
-    setUsers(data);
-    setLoading(false);
-  };
+  }, [fetchUsers]);
 
   if (!loading) {
     return (
