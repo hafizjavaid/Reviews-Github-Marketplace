cc97b4ab6905382382026970d2e47244fc789bce
hafizjavaid <hafizjaidi786@gmail.com>
2022-01-04 15:34:51 +0500

    Finishing Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import Header from "./components/Header";
-import "./index.css";
 import FeedbackList from "./components/FeedbackList";
-import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutIconLink from "./shared/AboutIconLink";
+import AboutPage from "./pages/AboutPage";
+
+import FeedbackData from "./data/FeedbackData";
+import "./index.css";
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
   const handleDelete = (id) => {
@@ -17,12 +21,29 @@ function App() {
   };
   return (
     <div>
-      <Header />
-      <div className="container">
-        <FeedbackForm addFeedback={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={handleDelete} />
-      </div>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <FeedbackForm addFeedback={addFeedback} />
+                  <FeedbackStats feedback={feedback} />
+                  <FeedbackList
+                    feedback={feedback}
+                    handleDelete={handleDelete}
+                  />
+                </>
+              }
+            ></Route>
+            <Route path="/about" element={<AboutPage />}></Route>
+          </Routes>
+          <AboutIconLink />
+        </div>
+      </Router>
     </div>
   );
 }
