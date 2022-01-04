5fd4468f6fba083d6140343901b716482dc4e2b8
hafizjavaid <hafizjaidi786@gmail.com>
2022-01-04 21:47:14 +0500

    Getting Users From State

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import './index.css'
+import "./index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
-// "server": "json-server --watch db.json --port 5000",
-// "dev": "concurrently \"npm run server\" \"npm start\""
