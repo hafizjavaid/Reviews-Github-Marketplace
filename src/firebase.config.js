ad4a808bc3a795756f98b7bc6f666f5c9067d530
hafizjavaid <hafizjaidi786@gmail.com>
2022-01-06 21:15:35 +0500

    Adding Assets

---
+++ src/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+import { getFirestore } from "firbase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBIstnJW75Q0CfREKKX3fN5skrXS0e5TvA",
+  authDomain: "house-marketplace-4b057.firebaseapp.com",
+  projectId: "house-marketplace-4b057",
+  storageBucket: "house-marketplace-4b057.appspot.com",
+  messagingSenderId: "456242399084",
+  appId: "1:456242399084:web:c5ef8bf1255cb1c4471549",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore();
